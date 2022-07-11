19703128f04f5c7e197d956fd8c69b0cf2add163
Alexander1N <76779050+Alexander1N@users.noreply.github.com>
2022-07-11 07:53:09 +0300

    Update main.go
    add INPUT_FOURGREETING

--- .github/actions/hello-world/main.go
+++ .github/actions/hello-world/main.go
@@ -15,6 +15,7 @@ thirdGreeting := os.Getenv("INPUT_THIRDGREETING")
 // Use those inputs in the actions logic
 fmt.Println("Hello " + firstGreeting)
 fmt.Println("Hello " + secondGreeting)
+fmt.Println("Hello " + os.Getenv("INPUT_FOURGREETING"))
 
 // Someimes inputs are not "required" and we can build around that
 if thirdGreeting != "" {
